0626e153fbdce8053bb3e47475481cb8593a48dd
jaribeiro <jaribeiro@CWBENGNPB4YTHP.scopus.com.br>
2019-06-17 16:13:17 -0300

    Initial load

---
+++ src/main/java/com/johnny/springmultipart/SpringMultipartApplication.java
@@ -0,0 +1,15 @@
+package com.johnny.springmultipart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(FileStoringProperties.class)
+public class SpringMultipartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultipartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnny/springmultipart/SpringMultipartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.johnny.springmultipart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMultipartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
